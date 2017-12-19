6361db3799e3508297aca25a3a06250e0d94b499
Zoltan Mathe <zoltan.mathe@cern.ch>
2017-12-19 16:29:34 +0100

    the multiple selector does not support inverse selection

--- WebApp/static/core/js/utils/DiracBoxSelect.js
+++ WebApp/static/core/js/utils/DiracBoxSelect.js
@@ -1,9 +1,14 @@
 Ext.define('Ext.dirac.utils.DiracBoxSelect', {
       extend : "Ext.form.field.Tag",
       requires : ["Ext.form.field.Tag"],
-      filterPickList: true,
+      filterPickList : true,
       multiSelect : true,
-      isInverseSelection : function(){
+      isInverseSelection : function() {
         return false;
+      },
+      setInverseSelection : function(bInverseSelection) {
+        if (bInverseSelection) {
+          Ext.Logger.warn("Invers selection is not supported in extjs 6!");
+        }
       }
     });
